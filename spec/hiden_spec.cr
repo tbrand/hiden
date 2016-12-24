15f94faa0e4336302ab7e2d5c08e70c3ef591274
tbrand <taichiro0709@gmail.com>
2016-12-24 21:45:05 +0900

    remote db_model

--- spec/hiden_spec.cr
+++ spec/hiden_spec.cr
@@ -1,37 +1,5 @@
 require "./spec_helper"
 require "./mock"
-require "db"
-
-db_model User, id : Int32, name : String
-
-describe "db_model" do
-  it "define model" do
-    user = User.new(1, "name")
-    user.id.should eq(1)
-    user.name.should eq("name")
-  end
-  
-  it "read model from row" do
-    user = User.read(MockRow)
-    user.id.should eq(1)
-    user.name.should eq("name")
-  end
-  
-  it "throw query and craete array of the models" do
-    users = User.query(MockDB, "dummy")
-    users.size.should eq(1)
-    users.first.id.should eq(1)
-    users.first.name.should eq("name")
-  end
-
-  it "create from json" do
-    json = "{\"id\":1,\"name\":\"name\"}"
-    user = User.from_json(json)
-    user.id.should eq(1)
-    user.name.should eq("name")
-    user.to_json.should eq(json)
-  end
-end
 
 describe "flash" do
   it "check result" do 
@@ -107,5 +75,7 @@ describe "redis_cache" do
   it "cache exists?" do
     MockCache.clean_cache
     MockCache.cache_exists?("dummy").should eq(false)
+    MockCache.set_cache("dummy", "ok")
+    MockCache.cache_exists?("dummy").should eq(true)
   end
 end
--- src/hiden.cr
+++ src/hiden.cr
@@ -1,45 +1,7 @@
-require "db"
 require "kemal"
 require "kemal-session"
 require "redis"
-
-macro db_model(name, *properties)
-  {% for p in properties %}
-    {% if !p.is_a?(TypeDeclaration) %}
-      raise "ERROR: Only type declaration is accepted for db_model"
-    {% end %}
-  {% end %}
-
-    record {{name.id}}, {{*properties}} do
-
-    JSON.mapping(
-      {% for p in properties %}
-        {{p.var}}: {{p.type}},
-      {% end %}
-    )
-
-    def self.query(db, q : String)
-      models = [] of self
-      
-      db.query(q) do |rows|
-        rows.each do
-          models << read(rows)
-        end
-      end
-      models
-    end
-    
-    def self.read(row)
-      new(
-        {% for p in properties %}
-          {% if p.is_a?(TypeDeclaration) %}
-            row.read({{p.type}}).as({{p.type}}),
-          {% end %}
-        {% end %}
-      )
-    end
-  end
-end
+require "db"
 
 class DebuggableDB
 
